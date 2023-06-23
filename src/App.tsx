69a920ec10c478b1c63e2b946bf742167668dc20
its-mohd-kaif <mohdkaif@cedcoss.com>
2023-06-23 12:24:06 +0530

    Add Error Boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Auth from "./components/auth/Auth";
 import { Routes, Route, Navigate, } from 'react-router-dom';
-import React, { Suspense } from "react";
+import React, { Suspense, Component, ReactNode, ErrorInfo, useState } from "react";
 import Panel from "./components/panel/Panel";
 import PrepareDashboard from "./components/auth/prepare/PrepareDashboard";
 import PrepareOnboarding from "./components/auth/prepare/PrepareOnboarding";
@@ -8,41 +8,78 @@ import { Toast, ToastWrapper } from "@cedcommerce/ounce-ui";
 import { useDispatch, useSelector } from "react-redux";
 import { hideToast } from "./redux/ReduxSlice";
 import Response from "./components/panel/onboarding/response/Response";
+import BrokenComponent from "./components/panel/utility/commonComponent/BrokenComponent";
+
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  state: ErrorBoundaryState = {
+    hasError: false,
+  };
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    console.error(error, errorInfo);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <FallbackComponent />;
+    }
+
+    return this.props.children;
+  }
+}
+
+const FallbackComponent: React.FC = () => (
+  <>
+    <BrokenComponent />
+  </>
+);
 
 
 function App() {
   const redux: any = useSelector((redux: any) => redux.redux)
   return (
-    <>
-      <Routes>
-        <Route
-          path="/auth/*"
-          element={
-            <Suspense fallback={<></>}>
-              <Auth />
-            </Suspense>
-          }>
-        </Route>
-        <Route path="*" element={<Navigate to={'/auth/login'} />} />
-        <Route path='/prepareOnboarding' element={<PrepareOnboarding />} />
-        <Route path='/prepareDashboard' element={<PrepareDashboard />} />
-        <Route path="/show/message" element={<Response />} />
-        <Route
-          path="/panel/:uId"
-          element={
-            <Suspense fallback={<></>}>
-              <Panel />
-            </Suspense>
-          }>
-          <Route path="*" element={<>NO Page Found 2</>} />
-        </Route>
-      </Routes>
-      <>
-        {redux.toast.type !== "" && redux.toast.message !== "" ?
-          <RenderToasts type={redux.toast.type} message={redux.toast.message} /> : null
-        }
-      </>
-    </>
+    <ErrorBoundary>
+      <React.Fragment>
+        <Routes>
+          <Route
+            path="/auth/*"
+            element={
+              <Suspense fallback={<></>}>
+                <Auth />
+              </Suspense>
+            }>
+          </Route>
+          <Route path="*" element={<Navigate to={'/auth/login'} />} />
+          <Route path='/prepareOnboarding' element={<PrepareOnboarding />} />
+          <Route path='/prepareDashboard' element={<PrepareDashboard />} />
+          <Route path="/show/message" element={<Response />} />
+          <Route
+            path="/panel/:uId"
+            element={
+              <Suspense fallback={<></>}>
+                <Panel />
+              </Suspense>
+            }>
+            <Route path="*" element={<>NO Page Found 2</>} />
+          </Route>
+        </Routes>
+        <>
+          {redux.toast.type !== "" && redux.toast.message !== "" ?
+            <RenderToasts type={redux.toast.type} message={redux.toast.message} /> : null
+          }
+        </>
+      </React.Fragment>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/components/panel/utility/commonComponent/BrokenComponent.tsx
@@ -0,0 +1,24 @@
+import { Button, FallBack, FlexLayout, TextLink, TextStyles } from '@cedcommerce/ounce-ui'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import BrokenPageSvg from '../../../../assets/images/svg/BrokenPageSvg'
+
+function BrokenComponent() {
+    const navigate = useNavigate()
+    return (
+        <div style={{ marginTop: "3em" }}>
+            <FallBack
+                action={<Button onClick={() => {
+                    navigate("auth/login")
+                    window.location.reload()
+                }} thickness="large" type="Primary">Go to Login</Button>}
+                illustration={<BrokenPageSvg />}
+                subTitle={<FlexLayout direction="vertical" halign="center"><TextStyles alignment="center" fontweight="normal" paragraphTypes="MD-1.4" textcolor="light" type="Paragraph" utility="none">Your file have some error. We’re trying to fix it.</TextStyles><p className="Paragraph inte-Align--center inte__text--light none inte__font--normal inte__Paragraph--font--medium  ">Need Help? Read our{' '}<TextLink label="Help Doc" onClick={function noRefCheck() { }} />{' '}for further details.</p></FlexLayout>}
+                title="Broken Page"
+            />
+        </div>
+
+    )
+}
+
+export default BrokenComponent
